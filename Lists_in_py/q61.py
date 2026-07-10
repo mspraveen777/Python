885085eb844007aa86dfbabd1511c82d74594f4e
mspraveen777 <mspraveen7777@gmail.com>
2026-07-10 14:46:32 +0530

    Day 2 q61

--- Lists_in_py/q61.py
+++ Lists_in_py/q61.py
@@ -2,3 +2,15 @@
 Q61. Given a list, remove all duplicate elements while preserving the original
 order of the unique items.
 """
+
+def remove_duplicates(lst):
+    new_lts = []
+    lst.sort()
+    n = len(lst)
+    for i in range(1, n):
+        if lst[i] not in new_lts:
+            new_lts.append(lst[i])
+    return new_lts
+lst = [10,10,50,10,20,20,20,30,30,30]
+ans = remove_duplicates(lst)
+print(ans)

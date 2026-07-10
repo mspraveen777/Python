27e23ed174c587f778bacf9ffcef4607e2dacc40
mspraveen777 <mspraveen7777@gmail.com>
2026-07-10 17:01:59 +0530

    Q67

--- Lists_in_py/q67.py
+++ Lists_in_py/q67.py
@@ -2,3 +2,6 @@
 Q67. Given a list of marks, use list comprehension to create a new list that contains
 only the marks that are above 75.
 """
+marks = [65,75,78,96,100,56,35,98,85,88]
+marks_above_75 = [num for num in marks if num > 75]
+print(marks_above_75)

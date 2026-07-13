860a94f10aa35d226ef89b0a21f10af45eb53060
mspraveen777 <mspraveen7777@gmail.com>
2026-07-13 13:14:57 +0530

    Q74

---
+++ tuples/q74.py
@@ -0,0 +1,10 @@
+#Create the tuple of 5 of your fav song and print first,middle and last using index
+
+fav_Songs = ("Munjane Manjali","Bisulu Kudire ondu","Googly","Aha Aodie Maya","Dorasane")
+n = len(fav_Songs)
+firstSong = fav_Songs[0]
+middleSong = fav_Songs[n//2]
+lastSong = fav_Songs[-1]
+print(f"First Song = {firstSong}")
+print(f"Middle Song = {middleSong}")
+print(f"last Song = {lastSong}")
